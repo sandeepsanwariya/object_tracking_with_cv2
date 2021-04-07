5c3747c85d786fbb6bccd2903200ad2988158cef
sandeep <53370280+sandeepsanwariya@users.noreply.github.com>
2021-04-07 20:22:00 +0530

    Add files via upload

---
+++ object_trcking.py
@@ -0,0 +1,29 @@
+import cv2
+cap=cv2.VideoCapture(0)
+
+
+#tracker=cv2.TrackerMOSSE_create()
+tracker=cv2.TrackerCSRT_create()
+success,img=cap.read()
+bbox=cv2.selectROI('tracking',img,False)
+tracker.init(img,bbox)
+
+def drawbox(img,bbox):
+    x,y,w,h=int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+    cv2.rectangle(img,(x,y),((x+w,y+h)),(255,0,0),3)
+
+
+while True:
+    timer=cv2.getTickCount()
+    success,img=cap.read()
+    success, bbox =tracker.update(img)
+    if success:
+        drawbox(img,bbox)
+    else:
+        cv2.putText(img,"LOST", (75, 75), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    fps=cv2.getTickFrequency()/(cv2.getTickCount()-timer)
+    cv2.putText(img,str(int(fps)),(75,50),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,0.7,(0,0,255),2)
+    cv2.imshow("tracking",img)
+    if cv2.waitKey(1)  & 0xFF ==ord("q"):
+        break
